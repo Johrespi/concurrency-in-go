a4504cad277df38159d95a4de4bca1bd56318546
Johrespi <johrespi@espol.edu.ec>
2025-10-16 23:05:26 -0500

    goroutine using a function variable

--- simple-anonymous-goroutine/simple-_anonymous-goroutine.go
+++ simple-anonymous-goroutine/simple-_anonymous-goroutine.go
@@ -1,14 +1,18 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
-func main(){
+func main() {
 
-	go func(){
+	go func() {
 		fmt.Println("Hello from a goroutine using an anonymous function")
 	}() // Note the () at the end to invoke the function immediately
 
 	// Do other things in main goroutine
+	anotherGoroutine := func() {
+		fmt.Println("Hello from another goroutine using a function variable")
+	}
+	go anotherGoroutine()
 }
