4b334346eac1878daa5be50a37625850990dd50f
Johrespi <johrespi@espol.edu.ec>
2025-10-17 00:03:04 -0500

    Update join-point.go

--- join-point/join-point.go
+++ join-point/join-point.go
@@ -5,6 +5,9 @@ import (
 	"sync"
 )
 
+// This program is deterministic due to the use of sync.WaitGroup to ensure
+// that the main goroutine waits for the spawned goroutine to finish before exiting.
+
 func main() {
 	var wg sync.WaitGroup
 
